e6b24d6da8c06c774860ee2e18e88cba6c180bfe
山椒鱼 <1078297543@QQ.COM>
2022-04-14 23:51:22 +0800

    山椒鱼的更新

---
+++ PLM/Library/Controls/Fileadd.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PLM.Library.Controls
+{
+    /// <summary>
+    /// Fileadd.xaml 的交互逻辑
+    /// </summary>
+    public partial class Fileadd : UserControl
+    {
+        public Fileadd()
+        {
+            InitializeComponent();
+        }
+
+        private void imgAdd_PointerDown(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Multiselect = false;
+            dialog.Filter = "tiff,psd(*.tiff,*.psd)|*.tiff;*.psd";
+            dialog.ShowDialog();
+            string foldPath = dialog.FileName;
+            imgAdd.Tag = "2";
+        }
+    }
+}
