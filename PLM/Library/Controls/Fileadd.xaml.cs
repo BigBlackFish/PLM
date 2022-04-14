0f5c4150c920a84d66fb28ebec4e44ffe8ab14c7
山椒鱼 <1078297543@QQ.COM>
2022-04-15 00:50:47 +0800

    上传文件页面的更新，以及上传文件的用户控件

--- PLM/Library/Controls/Fileadd.xaml.cs
+++ PLM/Library/Controls/Fileadd.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Win32;
+using PLM.Common;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -28,12 +29,45 @@ namespace PLM.Library.Controls
 
         private void imgAdd_PointerDown(object sender, EventArgs e)
         {
-            OpenFileDialog dialog = new OpenFileDialog();
-            dialog.Multiselect = false;
-            dialog.Filter = "tiff,psd(*.tiff,*.psd)|*.tiff;*.psd";
-            dialog.ShowDialog();
-            string foldPath = dialog.FileName;
-            imgAdd.Tag = "2";
+            //OpenFileDialog dialog = new OpenFileDialog();
+            //dialog.Multiselect = false;
+            //dialog.Filter = "tiff,psd(*.tiff,*.psd)|*.tiff;*.psd";
+            //dialog.ShowDialog();
+            //string foldPath = dialog.FileName;
+            if (imgAdd.Tag.ToString() == "0")
+            {
+                imgAdd.Tag = "1";
+            }
+            else
+            {
+                imgAdd.Tag = "0";
+            }
+        }
+
+
+        private void griDrag_Drop(object sender, DragEventArgs e)
+        {
+            string fileName = ((System.Array)e.Data.GetData(DataFormats.FileDrop)).GetValue(0).ToString();
+            ClassHelper.MessageAlert(ClassHelper.MainWindow.GetType(), 0, fileName);
+            if (imgAdd.Tag.ToString() == "0")
+            {
+                imgAdd.Tag = "1";
+            }
+            else
+            {
+                imgAdd.Tag = "0";
+            }
+        }
+
+        private void griDrag_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effects = DragDropEffects.Link;
+            } else
+            {
+                e.Effects = DragDropEffects.None;
+            }
         }
     }
 }
--- PLM/Library/Controls/FileaddRight.xaml.cs
+++ PLM/Library/Controls/FileaddRight.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using PLM.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +25,49 @@ namespace PLM.Library.Controls
         public FileaddRight()
         {
             InitializeComponent();
+        }
+
+        private void imgAdd_PointerDown(object sender, EventArgs e)
+        {
+            //OpenFileDialog dialog = new OpenFileDialog();
+            //dialog.Multiselect = false;
+            //dialog.Filter = "jpg,png(*.jpg,*.png)|*.jpg;*.png";
+            //dialog.ShowDialog();
+            //string foldPath = dialog.FileName;
+            if (imgAdd.Tag.ToString() == "0")
+            {
+                imgAdd.Tag = "2";
+            }
+            else
+            {
+                imgAdd.Tag = "0";
+            }
+        }
+
+        private void griDrag_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effects = DragDropEffects.Link;
+            }
+            else
+            {
+                e.Effects = DragDropEffects.None;
+            }
+        }
+
+        private void griDrag_Drop(object sender, DragEventArgs e)
+        {
+            string fileName = ((System.Array)e.Data.GetData(DataFormats.FileDrop)).GetValue(0).ToString();
+            ClassHelper.MessageAlert(ClassHelper.MainWindow.GetType(), 0, fileName);
+            if (imgAdd.Tag.ToString() == "0")
+            {
+                imgAdd.Tag = "2";
+            }
+            else
+            {
+                imgAdd.Tag = "0";
+            }
         }
     }
 }
