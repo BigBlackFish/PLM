256f6af018b907116b28d262b57f2edff2d4e700
山椒鱼 <1078297543@QQ.COM>
2022-04-15 21:22:32 +0800

    版面列表和正在上传的更新

---
+++ PLM/Library/Controls/UploadingDatagrid.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PLM.Library.Controls
+{
+    /// <summary>
+    /// UploadingDatagrid.xaml 的交互逻辑
+    /// </summary>
+    public partial class UploadingDatagrid : UserControl
+    {
+        public UploadingDatagrid()
+        {
+            InitializeComponent();
+        }
+    }
+}
