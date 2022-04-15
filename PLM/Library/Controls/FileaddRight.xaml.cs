3c2cef79d37ada577e685a9985bfadbf1235aa24
熙 王 <1324771795@qq.com>
2022-04-15 10:12:40 +0800

    wx  清理代码，方法名大写。

--- PLM/Library/Controls/FileaddRight.xaml.cs
+++ PLM/Library/Controls/FileaddRight.xaml.cs
@@ -1,19 +1,7 @@
-﻿using Microsoft.Win32;
-using PLM.Common;
+﻿using PLM.Common;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace PLM.Library.Controls
 {
@@ -27,7 +15,7 @@ namespace PLM.Library.Controls
             InitializeComponent();
         }
 
-        private void imgAdd_PointerDown(object sender, EventArgs e)
+        private void ImgAdd_PointerDown(object sender, EventArgs e)
         {
             //OpenFileDialog dialog = new OpenFileDialog();
             //dialog.Multiselect = false;
@@ -44,7 +32,7 @@ namespace PLM.Library.Controls
             }
         }
 
-        private void griDrag_DragEnter(object sender, DragEventArgs e)
+        private void GriDrag_DragEnter(object sender, DragEventArgs e)
         {
             if (e.Data.GetDataPresent(DataFormats.FileDrop))
             {
@@ -56,9 +44,9 @@ namespace PLM.Library.Controls
             }
         }
 
-        private void griDrag_Drop(object sender, DragEventArgs e)
+        private void GriDrag_Drop(object sender, DragEventArgs e)
         {
-            string fileName = ((System.Array)e.Data.GetData(DataFormats.FileDrop)).GetValue(0).ToString();
+            string fileName = ((Array)e.Data.GetData(DataFormats.FileDrop)).GetValue(0).ToString();
             ClassHelper.MessageAlert(ClassHelper.MainWindow.GetType(), 0, fileName);
             if (imgAdd.Tag.ToString() == "0")
             {
