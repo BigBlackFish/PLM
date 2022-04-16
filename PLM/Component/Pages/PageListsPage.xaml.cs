0e831e9762a6ba57efb65afa2075f86c713f89e9
山椒鱼 <1078297543@QQ.COM>
2022-04-17 04:14:27 +0800

    更新

--- PLM/Component/Pages/PageListsPage.xaml.cs
+++ PLM/Component/Pages/PageListsPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Controls;
+﻿using PLM.Models.ViewModels;
+using System.Windows.Controls;
 
 namespace PLM.Component.Pages
 {
@@ -7,9 +8,40 @@ namespace PLM.Component.Pages
     /// </summary>
     public partial class PageListsPage : Page
     {
+        private readonly PageListsPageViewModel viewModel;
         public PageListsPage()
         {
             InitializeComponent();
+            viewModel=DataContext as PageListsPageViewModel;
+        }
+
+        private void PageListsMain_Loaded(object sender, System.Windows.RoutedEventArgs e)
+        {
+            PageListsPageViewModel a = new PageListsPageViewModel();
+            viewModel.Files.Add(new PageFileListViewModel
+            {
+                Id = 154323,
+                Image = "/Library/Image/Jpg.png",
+                ImageInfomation = "版面一缩略图",
+                AssociationId = 1234556,
+                PageInfomation = "抛光油版面一面",
+                remarksinfomation = "用于新花色创建新的项目中？？？？？，用于新花色",
+                UploadDate = "2022-04-17 03:28:00",
+                Uploader = "BigBlackFish"
+            });
+            if (viewModel.Files.Count > 0)
+            {
+                viewModel.EmptyState = false;
+            }
+            else
+            {
+                viewModel.EmptyState = true;
+            }
+        }
+
+        private void PageListsMain_DataContextChanged(object sender, System.Windows.DependencyPropertyChangedEventArgs e)
+        {
+
         }
     }
 }
---
+++ PLM/Library/Controls/PageListItem.xaml.cs
@@ -0,0 +1,26 @@
+﻿using PLM.Models.ViewModels;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace PLM.Library.Controls
+{
+    /// <summary>
+    /// PageListItem.xaml 的交互逻辑
+    /// </summary>
+    public partial class PageListItem : UserControl
+    {
+        private PageFileListViewModel viewModel;
+        public PageListItem()
+        {
+            InitializeComponent();
+        }
+
+        private void UserControl_DataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (e.NewValue is PageFileListViewModel model)
+            {
+                viewModel = model;
+            }
+        }
+    }
+}
